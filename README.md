8894184403b83bd31075c300a3f3cd8142a687da
James Teow <james.teow83@gmail.com>
2020-12-12 11:39:52 -0500

    Updates

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 By default the implementation uses the layout shown in the demo screenshot and the original dataset file.
 
-One of the evaluation criterias is how the implementation will 'scale as more reports are added over time.' I created three other versions of the page which can be seen by editing the call to `reportsWidget.init()` in `main.js`.
+One of the criterias for evaluation is how the implementation will 'scale as more reports are added over time.' I created three other versions of the page which can be seen by editing the call to `reportsWidget.init()` in `main.js`. The goal was to provide alternatives derived from the original requirement.
 
 Import `datasetLarge.js` instead of `dataset.js` in `index.html` to see how the view looks with a larger number of reports.
 
@@ -16,7 +16,5 @@ And modify `mode` in `reportsWidget.init(data: Object, mode: String)` to see the
 ![alt text](demo_images/byDecade.png "Reports grouped by decade")
 `'byRecency'`: Shows the first six annual reports in a grid, and then groups the remaining reports in a list format to save screen real estate.
 ![alt text](demo_images/byRecency.png "Reports separated by most recent")
-`'byShowMore'`: Shows the first six annual reports, and hides others behind a button. Pressing the button will reveal another six annual reports.
+`'byShowMore'`: Shows the first six annual reports, and hides others behind a button. Pressing the button will reveal another six annual reports. The real implementation would avoid additional reports from being loaded until the user requests to do so.
 ![alt text](demo_images/byShowMore.png "Reports hidden beneath show more")
-
-In all of these cases, the goal was to provide an alternative that still kept to the spirit of the exercise which was to present the annual reports in a grid format.
--- src/js/main.js
+++ src/js/main.js
@@ -45,82 +45,78 @@ const reportsWidget = {
     },
 
     renderReports: function (data) {
-        const options = this.options;
-        $(options.layout).append('<div class=' + options.selector.slice(1) + '></div>')
-        $(options.selector).html(Mustache.render(options.template, data));
+        const opts = this.options;
+        $(opts.layout).append('<div class=' + opts.selector.slice(1) + '></div>')
+        $(opts.selector).html(Mustache.render(opts.template, data));
     },
 
     renderReportsByDecade: function (data) {
-        const selector = this.options.selector;
-        const selectorClass = selector.slice(1);
-        const layout = this.options.layout;
-        const template = this.options.template;
+        const opts = this.options;
+        const selectorClass = opts.selector.slice(1);
 
-        $(layout).append('<div class=' + selectorClass + '></div>');
-        let target = $(selector).last();
+        $(opts.layout).append('<div class=' + selectorClass + '></div>');
+        let target = $(opts.selector);
 
-        data.forEach(function(entry, index) {
+        data.forEach(function (entry, index) {
             const year = findYearInString(entry.title);
             if (index > 0 && year !== null) {
                 // If the year ends with a nine, subtract 9 to get the beginning of the decade
-                const begin = parseInt(year[0]) - 9;
-                $(layout).append('<h2 class="divider">' + begin + 's</h2><div class="' + selectorClass + '"></div>');
-                target = $(selector).last();
+                const decade = parseInt(year[0]) - 9;
+                $(opts.layout).append('<h2 class="divider">' + decade + 's</h2><div class="' + selectorClass + '"></div>');
+                target = $(opts.selector).last();
             }
-            $(target).append(Mustache.render(template, entry));
+            $(target).append(Mustache.render(opts.template, entry));
         });
     },
 
     renderReportsSegmentedByRecency: function (data) {
-        const selector = this.options.selector;
-        const selectorClass = selector.slice(1);
-        const layout = this.options.layout;
-        const template = this.options.template;
+        const opts = this.options;
+        const selectorClass = opts.selector.slice(1);
 
-        $(layout).append('<div class=' + selectorClass + '></div>');
-        let target = $(selector).last();
+        $(opts.layout).append('<div class=' + selectorClass + '></div>');
+        let target = $(opts.selector);
 
-        data.forEach(function(entry, index) {
+        data.forEach(function (entry, index) {
             if (index === UPPER_LIMIT) {
-                $(layout).append('<h2 class="divider">More Reports</h2><div class="' + selectorClass + ' list"></div>');
-                target = $(selector).last();
+                $(opts.layout).append('<h2 class="divider">More Reports</h2><div class="' + selectorClass + ' list"></div>');
+                target = $(opts.selector).last();
             }
-            $(target).append(Mustache.render(template, entry));
+            $(target).append(Mustache.render(opts.template, entry));
         });
     },
 
     renderWithShowMore: function (data) {
-        const selector = this.options.selector;
-        const selectorClass = selector.slice(1);
-        const layout = this.options.layout;
-        const template = this.options.template;
+        const opts = this.options;
+        const selectorClass = opts.selector.slice(1);
 
-        $(layout).append('<div class="' + selectorClass + ' current"></div>');
-        let target = $(selector).last();
+        $(opts.layout).append('<div class="' + selectorClass + ' current"></div>');
+        let target = $(opts.selector);
 
-        data.forEach(function(entry, index) {
+        data.forEach(function (entry, index) {
             if (index > 0 && (index % UPPER_LIMIT) === 0) {
-                $(layout).append('<div class="' + selectorClass + ' hide"></div>');
-                target = $(selector).last();
+                $(opts.layout).append('<div class="' + selectorClass + ' hide"></div>');
+                target = $(opts.selector).last();
             }
-            $(target).append(Mustache.render(template, entry));
+            $(target).append(Mustache.render(opts.template, entry));
         });
 
-        $(layout).append('<button id="next" class="button" type="button"> Show More </button>');
+        if (data.length > UPPER_LIMIT) {
+            $(opts.layout).append('<button id="next" class="button" type="button"> Show More </button>');
 
-        $('#next').click(function () {
-            const temp = $('.current');
-            const next = temp.next().slideDown({
-                start: function () {
-                    $(this).css({ display: "flex" });
-                    $(this).addClass('current');
+            $('#next').click(function () {
+                const temp = $('.current');
+                const next = temp.next().slideDown({
+                    start: function () {
+                        $(this).css({ display: 'flex' });
+                        $(this).addClass('current');
+                    }
+                });
+                temp.removeClass('current');
+                if (next.is(':nth-last-child(2)')) {
+                    $('#next').remove();
                 }
             });
-            temp.removeClass('current');
-            if (next.is(':nth-last-child(2)')) {
-                $('#next').remove();
-            }
-        });
+        }
 
     }
 
@@ -130,4 +126,4 @@ function findYearInString(str) {
     return str.match(YEAR_ENDING_IN_9);
 }
 
-reportsWidget.init(reportData || []);
+reportsWidget.init(reportData);
